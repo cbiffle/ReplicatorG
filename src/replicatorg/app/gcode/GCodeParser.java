cbiffle/ReplicatorG

commit 06a13f071209cd89b047319c6dbd10d673bce8ff

Modernized DriverQueryInterface.
While the Driver interface grew new methods to support multiple
tools, DriverQueryInterface was left behind.  Fortunately, it's
a much less popular type, so fixing this is a relatively small
change.  Because classes tend to implement both Driver and
DriverQueryInterface, and Driver already had the methods in
question, I was able to simply remove the old-style methods
from DriverQueryInterface.

As a side effect, a few G-Code commands are now multi-tool
aware -- specifically, the commands for setting spindle speed.

src/replicatorg/app/gcode/GCodeParser.java
@@ -438,7 +438,11 @@ public class GCodeParser {
 			break;
 		// read spindle speed
 		case M50:
-			driver.getSpindleRPM();
+			if (gcode.hasCode('T')) {
+				driver.getSpindleRPM((int) gcode.getCodeValue('P'));
+			} else {
+				driver.getSpindleRPM(driver.getCurrentToolIndex());
+			}
 			break;
 			// turn extruder on, forward
 		case M70:
@@ -493,19 +497,23 @@ public class GCodeParser {
 			commands.add(new replicatorg.drivers.commands.ReadTemperature());
 			break;
 		// turn AutomatedBuildPlatform on
-		case M106:
-			if(driver.hasAutomatedBuildPlatform())
+		case M106: {
+			int toolIndex = gcode.hasCode('T') ? (int) gcode.getCodeValue('T') : driver.getCurrentToolIndex();
+			if(driver.hasAutomatedBuildPlatform(toolIndex))
 				commands.add(new replicatorg.drivers.commands.ToggleAutomatedBuildPlatform(true));
 			else
 				commands.add(new replicatorg.drivers.commands.EnableFan());
 			break;
+		}
 		// turn AutomatedBuildPlatform off
-		case M107:
-			if(driver.hasAutomatedBuildPlatform())
+		case M107: {
+			int toolIndex = gcode.hasCode('T') ? (int) gcode.getCodeValue('T') : driver.getCurrentToolIndex();
+			if(driver.hasAutomatedBuildPlatform(toolIndex))
 				commands.add(new replicatorg.drivers.commands.ToggleAutomatedBuildPlatform(false));
 			else
 				commands.add(new replicatorg.drivers.commands.DisableFan());
 			break;
+		}
 		// set max extruder speed, RPM
 		case M108:
 			if (gcode.hasCode('S'))

src/replicatorg/app/ui/controlpanel/ExtruderPanel.java
@@ -274,7 +274,7 @@ public class ExtruderPanel extends JPanel{
 						}
 					}
 				}, "handleTextField", "motor-speed-pwm", 5, Base.getLocalFormat());
-				field.setValue(machine.getDriverQueryInterface().getMotorSpeedPWM());
+				field.setValue(machine.getDriverQueryInterface().getMotorSpeedPWM(tool.getIndex()));
 				panel.add(label);
 				panel.add(field,"wrap");
 			}

src/replicatorg/drivers/DriverBaseImplementation.java
@@ -398,6 +398,10 @@ public abstract class DriverBaseImplementation implements Driver, DriverQueryInt
 	@Override public void selectTool(int toolIndex) throws RetryException {
 		machine.selectTool(toolIndex);
 	}
+	
+	@Override public int getCurrentToolIndex() {
+		return machine.currentTool().getIndex();
+	}
 
 	/***************************************************************************
 	 * pause function
@@ -797,12 +801,7 @@ public abstract class DriverBaseImplementation implements Driver, DriverQueryInt
 	}
 
 	
-	@Override public boolean hasAutomatedBuildPlatform()
-	{
-		return hasAutomatedBuildPlatform(-1);
-	}
-
-	private boolean hasAutomatedBuildPlatform(int toolhead)
+	@Override public boolean hasAutomatedBuildPlatform(int toolhead)
 	{
 		/// toolhead -1 indicate auto-detect.Fast hack to get software out..
 		if(toolhead == -1 ) toolhead = machine.currentTool().getIndex();

src/replicatorg/drivers/DriverQueryInterface.java
@@ -11,31 +11,26 @@ import replicatorg.util.Point5d;
  * @author matt.mets
  */
 public interface DriverQueryInterface {
+	public int getCurrentToolIndex();
+	
 	public Point3d getOffset(int i);
 	
 	public Point5d getMaximumFeedrates();
 
-	public double getSpindleRPM();
+	public double getSpindleRPM(int toolhead);
 	
-	public double getMotorRPM();
+	public double getMotorRPM(int toolhead);
 	
-	public int getMotorSpeedPWM();
-
-	/** relies on timing to have the 'right selected toolhead', deprecated */
-	@Deprecated
-	public double getTemperature();
+	public int getMotorSpeedPWM(int toolhead);
 
 	public double getTemperature(int toolhead);
 
-	public double getTemperatureSetting();
+	public double getTemperatureSetting(int toolhead);
 
-	public boolean hasAutomatedBuildPlatform();
+	public boolean hasAutomatedBuildPlatform(int toolhead);
 	
-	/** relies on timing to have the 'right selected toolhead', deprecated */
-	@Deprecated
-	public double getPlatformTemperature();
 	public double getPlatformTemperature(int toolhead);
-	public double getPlatformTemperatureSetting();
+	public double getPlatformTemperatureSetting(int toolhead);
 
 	public Point5d getCurrentPosition(boolean b);
 

commit d6c714723bcfd8b559171e7c514a492e4a242101

Fixed a typo introduced in 06a13f07.
P and T sure sound a lot alike. :-\

src/replicatorg/app/gcode/GCodeParser.java
@@ -447,7 +447,7 @@ public class GCodeParser {
 		// read spindle speed
 		case M50:
 			if (gcode.hasCode('T')) {
-				driver.getSpindleRPM((int) gcode.getCodeValue('P'));
+				driver.getSpindleRPM((int) gcode.getCodeValue('T'));
 			} else {
 				driver.getSpindleRPM(driver.getCurrentToolIndex());
 			}